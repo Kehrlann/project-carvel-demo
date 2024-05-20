d59bc83cc2be0db03a268cbbde9a1367fa759002
Daniel Garnier-Moiroux <git@garnier.wf>
2024-05-20 16:43:32 +0200

    packaging-apis: rework to use the demo app

--- packaging-apis/setup.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
-imgpkg push --file "$SCRIPT_DIR/repository" --bundle dgarnier963/demo/carvel-package-repo:reference
